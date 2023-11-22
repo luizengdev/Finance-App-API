a7b26cbc823a7a2c90c52f44c5bf36c2e4e29967
luizengdev <luizengdev@gmail.com>
2023-11-22 18:33:43 -0300

    feat: add GetUserByIdControlle and validations

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -12,3 +12,13 @@ export const serverError = () => ({
     statusCode: 500,
     body: { message: 'Internal server error' },
 });
+
+export const ok = (body) => ({
+    statusCode: 200,
+    body,
+});
+
+export const notFound = () => ({
+    statusCode: 404,
+    body: { message: 'Not found' },
+});
