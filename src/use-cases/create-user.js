84b14f839350df169961fcf06b615483e5071a33
luizengdev <luizengdev@gmail.com>
2023-11-27 09:06:54 -0300

    feat: add PostgresGetUserByEmailRepository

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,10 +1,22 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js';
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
-        // TODO: verificar se o e-mail já está em uso
+        // verificar se o e-mail já está em uso
+        const postgresGetUserByEmailRepository =
+            new PostgresGetUserByEmailRepository();
+
+        const userWithProviderEmail =
+            await postgresGetUserByEmailRepository.execute(
+                createUserParams.email,
+            );
+
+        if (userWithProviderEmail) {
+            throw new Error('Email already in use');
+        }
 
         // gerar um ID do usario
         const userId = uuidv4();
