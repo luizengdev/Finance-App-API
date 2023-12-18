e149b029e2cf97450902301c6fb494541965bc77
luizengdev <luizengdev@gmail.com>
2023-12-18 18:57:07 -0300

    feat: add DeleteUserController

---
+++ src/controllers/delete-user.js
@@ -0,0 +1,29 @@
+import { DeleteUserUseCase } from '../use-cases/index.js';
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+} from './helpers/index.js';
+
+export class DeleteUserController {
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId;
+            const isIdValid = checkIfIdIsValid(userId);
+
+            if (!isIdValid) {
+                return invalidIdResponse();
+            }
+
+            const deleteUserUseCase = new DeleteUserUseCase();
+
+            const deletedUser = await deleteUserUseCase.execute(userId);
+
+            return ok(deletedUser);
+        } catch (error) {
+            console.error(error);
+            return serverError();
+        }
+    }
+}
