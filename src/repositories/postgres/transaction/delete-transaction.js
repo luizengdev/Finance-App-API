5f30c7115b503c52b59f6797555240058d41ca87
luizengdev <luizengdev@gmail.com>
2023-12-28 13:14:41 -0300

    feat: add PostgresDeleteTransactionRepository

---
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js';
+
+export class PostgresDeleteTransactionRepository {
+    async execute(transactionId) {
+        const deletedTransaction = await PostgresHelper.query(
+            'DELETE FROM transactions WHERE id = $1 RETURNING *',
+            [transactionId],
+        );
+
+        return deletedTransaction[0];
+    }
+}
