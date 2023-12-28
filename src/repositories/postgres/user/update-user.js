cbe64e2b6452ce2e8b3e9f0d27e72a8b6ce54970
luizengdev <luizengdev@gmail.com>
2023-12-28 09:49:05 -0300

    feat: add PostgresUpdateTransactionRepository

--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,9 +1,6 @@
 import { PostgresHelper } from '../../../db/postgres/helper.js';
 
 export class PostgresUpdateUserRepository {
-    // A função começa criando duas matrizes vazias: updateFields e updateValues. Em seguida, ela itera sobre as chaves do objeto updateUserParams
-    // e adiciona uma string formatada para cada chave à matriz updateFields. A string formatada é da forma ${key} = $${updateFields.length + 1}.
-    // Isso significa que o primeiro campo a ser atualizado terá o valor $1, o segundo $2, e assim por diante. A função também adiciona o valor correspondente a cada chave à matriz updateValues.
     async execute(userId, updateUserParams) {
         const updateFields = [];
         const updateValues = [];
@@ -22,7 +19,6 @@ export class PostgresUpdateUserRepository {
             RETURNING *
         `;
 
-        // Por fim, a função executa a consulta usando o método query do objeto PostgresHelper e retorna o registro atualizado.
         const updateUser = await PostgresHelper.query(
             updateQuery,
             updateValues,
